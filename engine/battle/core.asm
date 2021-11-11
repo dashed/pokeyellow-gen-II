83d90ef733e27f752c5de7ec69871504d4770a19
Alberto Leal <mail4alberto@gmail.com>
2021-11-10 23:11:31 -0500

    clean up 1/255 patch and over comment the entire thing.

--- engine/battle/core.asm
+++ engine/battle/core.asm
@@ -5613,22 +5613,27 @@ MoveHitTest:
 .doAccuracyCheck
 ; if the random number generated is greater than or equal to the scaled accuracy, the move misses
 ; note that this means that even the highest accuracy is still just a 255/256 chance, not 100%
+; Alberto - above was original bug; but should be patched below.
+; 
+; https://bulbapedia.bulbagarden.net/wiki/List_of_glitches_(Generation_I)#1.2F256_miss_glitch
+; https://github.com/pret/pokered/wiki/Fix-the-1-in-255-miss-bug
+; 
+; Convention:
+; If a randomly generated integer between 0 and 255 (inclusive) is less than or equal to the move's accuracy (after applying accuracy and evasion modifiers), the move hits.
 
-	; https://bulbapedia.bulbagarden.net/wiki/List_of_glitches_(Generation_I)#1.2F256_miss_glitch
-	; https://github.com/pret/pokered/wiki/Fix-the-1-in-255-miss-bug
 	; register b holds the move accuracy of the Pokémon currently using the move
-	; loads the contents of register b into register a
-	ld a, b
-	cp $FF ; Is the value $FF (255)? If so, then Z flag is set.
-	jr z, .move_hit ; If Z flag is set, then jump to .move_hit
+
+	; This is a condition check to be able to allow us to return early, without having to call BattleRandom.
+	ld a, b ; loads the contents of register b into register a
+	cp $FF ; If the value in register a is equal to $FF (255), then set Z flag.
+	ret z ; If Z flag is set, then return
 
 	call BattleRandom
-	cp b
+	; register a holds the prng result
+	cp b ; a - b
 	; https://github.com/jojobear13/shinpokered/blob/9d99e76da416d96fffa0c4b0bd583867371ad4b2/engine/battle/core.asm#L6124
-	jr z, .move_hit ;joenote - fixed move accuracy so a move hits if acc = randnum
-	jr nc, .moveMissed
-	ret
-.move_hit
+	ret z ; return if a == b
+	jr nc, .moveMissed ; move is missed if a >= b
 	ret
 .moveMissed
 	xor a

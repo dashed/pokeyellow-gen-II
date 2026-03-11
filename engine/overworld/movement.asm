17e6eea45b5aa86cdf66124a3b2ff5250b35689d
Alberto Leal <mail4alberto@gmail.com>
2026-03-11 10:52:30 -0400

    fix(overworld): add carry propagation to UpdateNPCSprite movement byte lookup

--- engine/overworld/movement.asm
+++ engine/overworld/movement.asm
@@ -104,6 +104,9 @@ UpdateNPCSprite:
 	ld hl, wMapSpriteData
 	add l
 	ld l, a
+	jr nc, .noCarry
+	inc h
+.noCarry
 	ld a, [hl]        ; read movement byte 2
 	ld [wCurSpriteMovement2], a
 	ld h, HIGH(wSpriteStateData1)

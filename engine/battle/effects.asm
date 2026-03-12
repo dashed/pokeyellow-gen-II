8e093bcc79b05e41bc46656bd74bd418d7ebc543
Alberto Leal <mail4alberto@gmail.com>
2026-03-11 22:21:28 -0400

    fix(battle): clear Hyper Beam recharge when enemy inflicts freeze

--- engine/battle/effects.asm
+++ engine/battle/effects.asm
@@ -329,7 +329,7 @@ FreezeBurnParalyzeEffect:
 	ld hl, BurnedText
 	jp PrintText
 .freeze2
-; hyper beam bits aren't reset for opponent's side
+	call ClearHyperBeam ; fix: clear recharge bit to prevent Haze freeze softlock
 	ld a, 1 << FRZ
 	ld [wBattleMonStatus], a
 	ld a, SHAKE_SCREEN_ANIM
